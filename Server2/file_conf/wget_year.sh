26d81227429fb690b8e9e34e6eb4ff4aa6dbdc45
FFat15 <35541823+FFat15@users.noreply.github.com>
2020-07-06 14:49:19 -0500

    Ahora recupera los datos del 2020

--- Server2/file_conf/wget_year.sh
+++ Server2/file_conf/wget_year.sh
@@ -2,22 +2,22 @@
 
 
 j=2;
-for i in $(seq 1 11)
+for i in $(seq 1 6)
 	do
 		#echo ${i}
 		j=$(expr $i + 1)
 		#echo ${j}
 		if [ $i -eq 1 ]
 		    then
-			     wget 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime=2018-'${i}'-01&endtime=2018-'${i}'-14'
-			     wget 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime=2018-'${i}'-15&endtime=2018-'${j}'-01'
+			     wget 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime=2020-'${i}'-01&endtime=2018-'${i}'-14'
+			     wget 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime=2020-'${i}'-15&endtime=2018-'${j}'-01'
 		else
-		       wget 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime=2018-'${i}'-02&endtime=2018-'${i}'-14'
-		       wget 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime=2018-'${i}'-15&endtime=2018-'${j}'-01'
+		       wget 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime=2020-'${i}'-02&endtime=2018-'${i}'-14'
+		       wget 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime=2020-'${i}'-15&endtime=2018-'${j}'-01'
 		fi
 	done
 
-wget 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime=2018-12-02&endtime=2018-12-15'
-wget 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime=2018-12-16&endtime=2018-12-31'
 
-cat query* > 206_2018.csv
+
+cat query* > 2020.csv
+rm query*
